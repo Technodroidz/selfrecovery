7345c5d3191f135f48f4f3cdcafc753511136701
ayush <79449782+Technodroidz@users.noreply.github.com>
2023-06-01 13:06:16 +0530

    new

--- src/front/Designs.js
+++ src/front/Designs.js
@@ -1,8 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import Modal from "react-bootstrap/Modal";
-import Multiselect from 'multiselect-react-dropdown';
 import Accordion from 'react-bootstrap/Accordion';
 import Header from '../front/Header';
 import Footer from '../front/Footer';
